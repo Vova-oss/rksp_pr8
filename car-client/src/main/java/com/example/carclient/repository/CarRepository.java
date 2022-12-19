1fb4685dafc411e515c65cd2e2f7fe33423990dc
Полетаев Владимир Викторович <ogyrcka@gmail.com>
2022-12-19 22:42:44 +0300

    Second try Delete all

--- car-client/src/main/java/com/example/carclient/repository/CarRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.carclient.repository;
-
-import com.example.carclient.entity.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CarRepository extends JpaRepository<Car, Long> {
-}
--- init.sql
+++
@@ -1,8 +0,0 @@
-insert into pr8_car(model, price) VALUES ('Ferarry', '9999'), ('BMW','1111');
-
-insert into pr8_people(age,"name") values (10, 'Vova'), (20, 'Misha'), (30, 'Kolya');
-
-
-docker-compose up eureka-server dbpeople dbcar -d
-docker-compose up car-client people-client feign-client -d
-docker-compose up api-gateway -d
